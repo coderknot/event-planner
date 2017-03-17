d358cea0325fc584dcced721b752317cb5bf2de0
CoderKnot <coder.knot@gmail.com>
2017-03-17 08:50:34 -0700

    create test for instantiation of Event object

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -2,5 +2,10 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class EventTest {
-  
+
+    @Test
+    public void newEvent_instantiatesCorrectly() {
+      Event testEvent = new Event(10, 1, 1, 1);
+      assertTrue(testEvent instanceof Event);
+    }
 }
