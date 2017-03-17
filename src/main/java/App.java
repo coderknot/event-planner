a7b62f491b06c756b586fb149ac339eedc68da4e
CoderKnot <coder.knot@gmail.com>
2017-03-17 11:21:40 -0700

    create basic CLI that creates an Event with test values and return the Event cost

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -3,7 +3,20 @@ import java.io.Console;
 public class App {
 
   public static void main(String[] args) {
-    
+    Console console = System.console();
+
+    int numberOfGuests = 10;
+    int foodChoice = 1;
+    int beverageChoice = 1;
+    int entertainmentChoice = 1;
+    String[] couponCodes = {"10-OFF-100"};
+
+    System.out.println("Event Planner");
+
+    Event newEvent = new Event(numberOfGuests, foodChoice, beverageChoice, entertainmentChoice, couponCodes);
+
+    System.out.println("This event will cost: $" + newEvent.calculateCost());
+
   }
 
 }
