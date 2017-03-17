a360947f6d76e54c9b6865a8b97c2d82012c2c55
CoderKnot <coder.knot@gmail.com>
2017-03-17 14:27:51 -0700

    refactor CLI, updating appearance

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,33 +16,33 @@ public class App {
   public static Event createEvent() {
     Console console = System.console();
 
-    System.out.println("New Event:");
+    System.out.println("\nNew Event:");
 
-    System.out.println("How many guests will be attending?");
+    System.out.println("\nHow many guests will be attending?");
     int numberOfGuests = Integer.parseInt(console.readLine());
 
-    System.out.println("What type of food is being served?");
-    System.out.println("1. Light Snacks ($2 per guest)");
+    System.out.println("\nWhat type of food is being served?");
+    System.out.println("\n1. Light Snacks ($2 per guest)");
     System.out.println("2. Meal ($3 per guest)");
     System.out.println("3. Meal + Desert ($4 per guest)");
 
     int foodChoice = Integer.parseInt(console.readLine());
 
-    System.out.println("What type of beverages are being served?");
-    System.out.println("1. Water, Juice, and Soda ($1 per guest)");
+    System.out.println("\nWhat type of beverages are being served?");
+    System.out.println("\n1. Water, Juice, and Soda ($1 per guest)");
     System.out.println("2. Option 1 + Light Bar ($2 per guest)");
     System.out.println("3. Option 1 + Full Bar ($3 per guest)");
 
     int beverageChoice = Integer.parseInt(console.readLine());
 
-    System.out.println("What type of entertainment is being provided?");
-    System.out.println("1. Live Band ($5 per guest)");
+    System.out.println("\nWhat type of entertainment is being provided?");
+    System.out.println("\n1. Live Band ($5 per guest)");
     System.out.println("2. DJ ($3 per guest)");
     int entertainmentChoice = Integer.parseInt(console.readLine());
 
-    System.out.println("Would you like to apply a coupon? Separate coupon codes with a space.");
-    System.out.println("10-OFF-100");
-    System.out.println("5-OFF-150");
+    System.out.println("\nWould you like to apply a coupon? Separate coupon codes with a space.");
+    System.out.println("\n* 10-OFF-100");
+    System.out.println("* 5-OFF-150");
     String couponCodes = console.readLine();
     String[] couponCodesArray = couponCodes.split(" ");
 
@@ -52,8 +52,8 @@ public class App {
   }
 
   public static void displayEvent(Event event) {
-    System.out.println("Here are the details of your event:\n");
-    System.out.println("Number of guests: " + event.getNumberOfGuests());
+    System.out.println("\nHere are the details of your event: ");
+    System.out.println("\nNumber of guests: " + event.getNumberOfGuests());
     System.out.println("Food served: " + event.getFoodChoice());
     System.out.println("Beverages served: " + event.getBeverageChoice());
     System.out.println("Entertainment option: " + event.getEntertainmentChoice());
@@ -64,6 +64,6 @@ public class App {
     }
 
     System.out.println("Coupon codes: " + couponCodes);
-    System.out.println("Total cost: $" + event.calculateCost());
+    System.out.println("\nTotal cost: $" + event.calculateCost());
   }
 }
