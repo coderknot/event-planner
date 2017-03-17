54e80654342b437d451a3c36269cb76ad76d0f84
CoderKnot <coder.knot@gmail.com>
2017-03-17 08:59:41 -0700

    create method to return number of guests

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,6 +1,10 @@
 public class Event {
 
     public Event(int numberOfGuests, int foodChoice, int beverageChoice, int entertainmentChoice) {
-      
+
+    }
+
+    public int getNumberOfGuests() {
+      return 0;
     }
 }
