c06ee8b7beed9fbd99d61302817c4a8142653d6b
CoderKnot <coder.knot@gmail.com>
2017-03-17 09:46:34 -0700

    refactor test to calculate event cost to include food choice

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -36,6 +36,6 @@ public class EventTest {
     @Test
     public void calculateCost_calculateEventCost_50() {
       Event testEvent = new Event(10, 1, 1, 1);
-      assertEquals(50, testEvent.calculateCost());
+      assertEquals(70, testEvent.calculateCost());
     }
 }
